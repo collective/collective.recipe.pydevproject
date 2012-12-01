dcce0afa6af18300d91c5b0f33fa6f977e69453d
Marcio Mazza <marciomazza@gmail.com>
2012-12-01 19:00:30 -0200

    Minimize test egg stubs

--- collective/recipe/pydevproject/tests/eggstub.dep/setup.py
+++ collective/recipe/pydevproject/tests/eggstub.dep/setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(name='eggstub.dep',
-      package_dir = {'': 'src'},
       install_requires=[
           # NOTHING REQUIRED
       ],
--- collective/recipe/pydevproject/tests/eggstub.main/setup.py
+++ collective/recipe/pydevproject/tests/eggstub.main/setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(name='eggstub.main',
-      package_dir = {'': 'src'},
       install_requires=[
           'eggstub.dep', #### THIS EGG DEPENDS ON ANOTHER ####
       ],
